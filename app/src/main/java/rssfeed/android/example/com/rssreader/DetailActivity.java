39102f36ab81177ca87ccd88b193e24813141d88
nutthaphon <nutthaphon@gmail.com>
2015-04-18 22:36:04 +0700

    portrait mode

--- app/src/main/java/rssfeed/android/example/com/rssreader/DetailActivity.java
+++ app/src/main/java/rssfeed/android/example/com/rssreader/DetailActivity.java
@@ -1,25 +1,41 @@
 package rssfeed.android.example.com.rssreader;
 
-import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class RssfeedActivity extends ActionBarActivity implements MyListFragment.OnItemSelectedListener {
+public class DetailActivity extends ActionBarActivity {
+
+    public static final String EXTRA_URL = "url";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rssfeed);
+
+        // Need to check if Activity has been switched to landscape mode
+        // If yes, finished and go back to the start Activity
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            finish();
+            return;
+        }
+        setContentView(R.layout.activity_detail);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String url = extras.getString(EXTRA_URL);
+            DetailFragment detailFragment = (DetailFragment) getFragmentManager()
+                    .findFragmentById(R.id.detailFragment);
+            detailFragment.setText(url);
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_rssfeed, menu);
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }
 
@@ -36,17 +52,5 @@ public class RssfeedActivity extends ActionBarActivity implements MyListFragment
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onRssItemSelected(String link) {
-        DetailFragment fragment = (DetailFragment) getFragmentManager().findFragmentById(R.id.detailFragment);
-        if (fragment != null && fragment.isInLayout()) {
-            fragment.setText(link);
-        } else {
-            Intent intent = new Intent(getApplicationContext(),DetailActivity.class);
-            intent.putExtra(DetailActivity.EXTRA_URL, link);
-            startActivity(intent);
-        }
     }
 }
